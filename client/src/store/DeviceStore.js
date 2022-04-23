3caa30d4c8a3338656c492f73bf75e57f7f2c65d
Extroutor <ahmedova.leila@list.ru>
2022-04-23 16:49:25 +0300

    Add admin panel, shop page and device page

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,18 +4,30 @@ export default class DeviceStore {
     constructor() {
         this._types = [
             {id: 1, name: "Холодильники"},
-            {id: 2, name: "Смартфоны"}
+            {id: 2, name: "Смартфоны"},
+            {id: 3, name: "Ноутбуки"},
+            {id: 4, name: "Телевизоры"}
         ]
         this._brands = [
             {id: 1, name: "Samsung"},
-            {id: 2, name: "Apple"}
+            {id: 2, name: "Apple"},
+            {id: 3, name: "Lenovo"},
+            {id: 4, name: "Asus"}
         ]
         this._devices = [
             {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
-            {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
-            {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
-            {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'}
-        ]
+            {id: 2, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
+            {id: 3, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
+            {id: 4, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
+            {id: 5, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
+            {id: 6, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
+            {id: 7, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
+            {id: 8, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
+            {id: 9, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'},
+            {id: 10, name: "Iphone 12 pro", price: 25000, rating: 5, img: '9922ee81-db5f-4b74-9470-c2f45ac08881.jpg'}        ]
+        this._selectedType = {}
+        this._selectedBrand = {}
+
         makeAutoObservable(this)
     }
 
@@ -31,11 +43,11 @@ export default class DeviceStore {
     }
 
     setSelectedType(type) {
-        this.setPage(1)
+        // this.setPage(1)
         this._selectedType = type
     }
     setSelectedBrand(brand) {
-        this.setPage(1)
+        // this.setPage(1)
         this._selectedBrand = brand
     }
     setPage(page) {
